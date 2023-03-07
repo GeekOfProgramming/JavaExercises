374521e320132045e2d6b15dc197ae0897bd05eb
Hamid <70663312+GeekOfProgramming@users.noreply.github.com>
2023-03-07 19:45:24 +0100

    Delete Exercise directory

--- Exercise/InterfaceExercise/Resizable.java
+++
@@ -1,7 +0,0 @@
-package Exercise.InterfaceExercise;
-
-public interface Resizable {
-    
-    @Override
-    public void resize(int aPercent);
-}
--- Exercise/Shape/TestRectangle.java
+++
@@ -1,5 +0,0 @@
-public class TestRectangle {
-    public static void main(String[] args) {
-        System.out.println(" ********Testing Rectangle class********");
-    }
-}
